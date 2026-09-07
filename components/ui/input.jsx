a03da5b1d470b40a4cc53866b74a64d443c8f9ad
Maateja <naikmteja@gmail.com>
2026-09-08 02:05:15 +0530

    removed footer

--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -2,6 +2,13 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+/**
+ * @typedef {React.InputHTMLAttributes<HTMLInputElement>} InputProps
+ */
+
+/**
+ * @type {React.ForwardRefExoticComponent<InputProps & React.RefAttributes<HTMLInputElement>>}
+ */
 const Input = React.forwardRef(({ className = "", type = "text", ...props }, ref) => {
   return (
     <input
